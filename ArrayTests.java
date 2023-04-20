d553e584e3287dbcecd307920ebd49067a0a0422
mk7652 <130100151+mk7652@users.noreply.github.com>
2023-04-20 13:05:21 -0700

    hi

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,27 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] input2 = {};
+    int[] input3 = {1, 2, 3};
+    int[] input4 = {1, 2, 3, 4};
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
+    ArrayExamples.reverseInPlace(input3);
+    ArrayExamples.reverseInPlace(input4);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{}, input2);
+    assertArrayEquals(new int[]{3, 2, 1}, input3);
+    assertArrayEquals(new int[]{4, 3, 2, 1}, input4);
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] input2 = {1, 2, 3};
+    int[] input3 = {1, 2, 3, 4};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input2));
+    assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input3));
   }
 }
